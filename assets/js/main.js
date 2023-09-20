8063d743efa15e4252912576a4a7ee56b5cf44ba
devgabrielchagas <gabriel.chagas2044@gmail.com>
2023-09-20 16:34:16 -0300

    put: reduzindo verbosidade e separando consumo da API da manipulaçao HTML

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,3 @@
-const offset = 0;
-const limit = 10;
-
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
-
 function convertPokemonToList(pokemon) {
     return ` 
     <li class="pokemon">
@@ -23,14 +18,7 @@ function convertPokemonToList(pokemon) {
 
 const pokemonList = document.getElementById('pokemonList')
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => {
-        for (let i = 0; i < pokemons.length; i++) {
-            const pokemon = pokemons[i];
-            pokemonList.innerHTML += convertPokemonToList(pokemon) 
-        }
-    })
-    .catch((error) => console.error(error))
-    .finally(() => console.log('Requisição concluída.'))
+pokeAPI.getPokemons().then((pokemons) => {
+    pokemonList.innerHTML += pokemons.map(convertPokemonToList).join('')
+}).catch((error) => console.error(error))
+
